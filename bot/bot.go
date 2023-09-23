2a605e6046d4b3b97b245a41742d55891f89a186
johannes <johannes.herman@gmail.com>
2023-09-23 02:25:24 +0200

    simple map downloader

---
+++ bot/bot.go
@@ -0,0 +1,92 @@
+package bot
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"strings"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var (
+	BotToken string
+	BotMapDir string
+)
+
+func Run() { 
+	discord, err := discordgo.New("Bot " + BotToken)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	discord.AddHandler(newMessage)
+	discord.Open()
+
+	defer discord.Close()
+
+	fmt.Println("Bot running ...")
+	
+	// Wait for os interrupt
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+}
+
+func newMessage(discord *discordgo.Session, message *discordgo.MessageCreate) {
+	if message.Author.ID == discord.State.User.ID {
+		return
+	}
+	switch {
+	case len(message.Attachments) > 0:	
+		names := downloadMaps(message.Attachments)
+		discord.ChannelMessageSend(message.ChannelID, "Successfully uploaded " + strings.Join(names, ", "))
+	case strings.Contains(message.Content, "ping"):
+		discord.ChannelMessageSend(message.ChannelID, "pong !")	
+	}
+}
+
+func downloadMaps(attachments []*discordgo.MessageAttachment) ([]string) {
+	var successNames []string
+	for _, attachment := range attachments {
+		if strings.HasSuffix(attachment.Filename, ".map") {
+			success := downloadMap(attachment)
+			if success {
+				successNames = append(
+					successNames, 
+					strings.TrimSuffix(
+						attachment.Filename, 
+					filepath.Ext(attachment.Filename)))
+			}
+		}
+	}
+	return successNames
+}
+
+func downloadMap(attachment *discordgo.MessageAttachment) (bool) {
+	stat := true
+	res, err := http.DefaultClient.Get(attachment.URL)
+	if err != nil {
+		log.Println("Could not get attachment from URL")
+		stat = false
+	}
+	defer res.Body.Close()
+
+	out, err := os.Create(BotMapDir + attachment.Filename)
+	if err != nil {
+		log.Println("Could not create file")
+		stat = false
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, res.Body)
+	if err != nil  {
+		log.Println("Could not copy res to out")
+		stat = false
+	}
+	return stat
+}
--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-ckage main
+package main
 
 import (
 	"rrt-bot/bot"
@@ -6,5 +6,17 @@ import (
 	"os"
 )
 
-func main() {
-	botToken, ok := os.LookupEnv("BOOT_TOKEN")
+func main() { 
+	botToken, ok := os.LookupEnv("BOT_TOKEN")
+	if !ok {
+		log.Fatal("Env variable BOT_TOKEN not found")
+	}
+	botMapDir, ok := os.LookupEnv("BOT_MAP_DIR")
+	if !ok {
+		log.Fatal("Env variable BOT_MAP_DIR not found")
+	}	
+	
+	bot.BotToken = botToken
+	bot.BotMapDir = botMapDir
+	bot.Run()
+}
